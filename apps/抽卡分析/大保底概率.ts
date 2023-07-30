a27a208a98accf0eb296e47de3829495ee69fb64
fantasy-hx <541531459@qq.com>
2023-07-30 19:01:47 +0800

    优化

--- apps/抽卡分析/大保底概率.ts
+++ apps/抽卡分析/大保底概率.ts
@@ -99,13 +99,54 @@ export class fenxichouka1 extends plugin {
           sixStarPullsOverall += sixStarPullsInPool
           misfitPullsOverall += misfitPullsInPool
         }
-      })
 
-      // 输出总概率
-      const overallSixStarProbability = (sixStarPullsOverall / totalPullsOverall) * 100
-      const overallMisfitProbability = (misfitPullsOverall / sixStarPullsOverall) * 100
+        // 歪卡数量和不歪卡数量的初始化
+        let misfitCount = 0
+        let notMisfitCount = 0
 
-      /*
+        // 判断首尾六星是否歪了
+        const firstPull = file2Data[0].results[0]
+        const lastPull = file2Data[file2Data.length - 1].results[0]
+        const specialSixStar = 0
+        const isFirstMisfit = firstPull.includes('6星') && !firstPull.includes(specialSixStar)
+        const isLastMisfit = lastPull.includes('6星') && !lastPull.includes(specialSixStar)
+
+        // 统计歪卡数量和不歪卡数量
+        Object.keys(poolStats).forEach(poolName => {
+          const { sixStarPulls, misfitPulls } = poolStats[poolName]
+          misfitCount += misfitPulls
+          notMisfitCount += sixStarPulls - misfitPulls
+        })
+        // 根据不同情况计算歪卡的概率
+        let overallMisfitProbability = 0
+        if (isFirstMisfit && isLastMisfit) {
+          if (misfitCount <= notMisfitCount) {
+            overallMisfitProbability = misfitCount / (sixStarPullsOverall - misfitCount + 1)
+          } else {
+            overallMisfitProbability = 1
+          }
+        } else if (isFirstMisfit && !isLastMisfit) {
+          if (misfitCount < notMisfitCount) {
+            overallMisfitProbability = misfitCount / (sixStarPullsOverall - misfitCount)
+          } else {
+            overallMisfitProbability = 1
+          }
+        } else if (!isFirstMisfit && isLastMisfit) {
+          if (misfitCount < notMisfitCount) {
+            overallMisfitProbability = misfitCount / (sixStarPullsOverall - misfitCount)
+          }
+        } else {
+          if (misfitCount < notMisfitCount || misfitCount > notMisfitCount) {
+            overallMisfitProbability = misfitCount / (sixStarPullsOverall - misfitCount)
+          } else {
+            overallMisfitProbability = 1
+          }
+        }
+
+        // 歪卡的概率
+        overallMisfitProbability *= 100
+
+        /*
 
 
 大保底概率图片合成
@@ -113,147 +154,148 @@ export class fenxichouka1 extends plugin {
 
       */
 
-      async function generateTextImage(text, ttffontPath, width, height) {
-        const canvas = createCanvas(width, height)
-        const ctx = canvas.getContext('2d')
+        async function generateTextImage(text, ttffontPath, width, height) {
+          const canvas = createCanvas(width, height)
+          const ctx = canvas.getContext('2d')
 
-        // 注册外部字体
-        registerFont(ttffontPath, { family: 'MyFont' })
+          // 注册外部字体
+          registerFont(ttffontPath, { family: 'MyFont' })
 
-        // 绘制文字
-        ctx.font = '264.67px MyFont'
-        ctx.fillStyle = '#545454'
-        ctx.textAlign = 'center' // 居中对齐文本
-        ctx.fillText(text, width / 2, height / 2)
+          // 绘制文字
+          ctx.font = '264.67px MyFont'
+          ctx.fillStyle = '#545454'
+          ctx.textAlign = 'center' // 居中对齐文本
+          ctx.fillText(text, width / 2, height / 2)
 
-        // 将Canvas生成的Buffer转换为Node.js的Buffer类型
-        const buffer = canvas.toBuffer()
+          // 将Canvas生成的Buffer转换为Node.js的Buffer类型
+          const buffer = canvas.toBuffer()
 
-        return buffer
-      }
-
-      async function selectGradeImage(overallMisfitProbability) {
-        // 映射表，将大保底概率范围与对应的图片路径进行关联
-        const gradeImageMap = {
-          'SS': 'SS.png',
-          'S': 'S.png',
-          'A+': 'A+.png',
-          'A': 'A.png',
-          'B+': 'B+.png',
-          'B': 'B.png',
-          'C+': 'C+.png',
-          'C': 'C.png'
+          return buffer
         }
 
-        let gradeImage
-        if (overallMisfitProbability >= 0 && overallMisfitProbability < 10) {
-          gradeImage = gradeImageMap['SS']
-        } else if (overallMisfitProbability >= 10 && overallMisfitProbability < 20) {
-          gradeImage = gradeImageMap['S']
-        } else if (overallMisfitProbability >= 20 && overallMisfitProbability < 30) {
-          gradeImage = gradeImageMap['A+']
-        } else if (overallMisfitProbability >= 30 && overallMisfitProbability < 40) {
-          gradeImage = gradeImageMap['A']
-        } else if (overallMisfitProbability >= 40 && overallMisfitProbability < 50) {
-          gradeImage = gradeImageMap['B+']
-        } else if (overallMisfitProbability >= 50 && overallMisfitProbability < 60) {
-          gradeImage = gradeImageMap['B']
-        } else if (overallMisfitProbability >= 60 && overallMisfitProbability < 70) {
-          gradeImage = gradeImageMap['C+']
-        } else if (overallMisfitProbability >= 70 && overallMisfitProbability < 100) {
-          gradeImage = gradeImageMap['C']
-        } else {
-          // 默认情况，当大保底概率超出映射表的范围时，默认为C.png
-          gradeImage = gradeImageMap['C']
-        }
-        return gradeImage
-      }
+        async function selectGradeImage(overallMisfitProbability) {
+          // 映射表，将大保底概率范围与对应的图片路径进行关联
+          const gradeImageMap = {
+            'SS': 'SS.png',
+            'S': 'S.png',
+            'A+': 'A+.png',
+            'A': 'A.png',
+            'B+': 'B+.png',
+            'B': 'B.png',
+            'C+': 'C+.png',
+            'C': 'C.png'
+          }
 
-      async function addTextAndSpecialImagesToBackground(
-        text,
-        backgroundImagePath,
-        fontPath,
-        outputImagePath
-      ) {
-        try {
-          // 使用 Jimp 读取背景图片
-          const backgroundImage = await jimp.read(backgroundImagePath)
-          // 根据大保底概率选择合适的图片路径
-          const gradeImageFileName = await selectGradeImage(overallMisfitProbability)
-          const gradeImagePath = path.join(
+          let gradeImage
+          if (overallMisfitProbability >= 0 && overallMisfitProbability < 10) {
+            gradeImage = gradeImageMap['SS']
+          } else if (overallMisfitProbability >= 10 && overallMisfitProbability < 20) {
+            gradeImage = gradeImageMap['S']
+          } else if (overallMisfitProbability >= 20 && overallMisfitProbability < 30) {
+            gradeImage = gradeImageMap['A+']
+          } else if (overallMisfitProbability >= 30 && overallMisfitProbability < 40) {
+            gradeImage = gradeImageMap['A']
+          } else if (overallMisfitProbability >= 40 && overallMisfitProbability < 50) {
+            gradeImage = gradeImageMap['B+']
+          } else if (overallMisfitProbability >= 50 && overallMisfitProbability < 60) {
+            gradeImage = gradeImageMap['B']
+          } else if (overallMisfitProbability >= 60 && overallMisfitProbability < 70) {
+            gradeImage = gradeImageMap['C+']
+          } else if (overallMisfitProbability >= 70 && overallMisfitProbability < 100) {
+            gradeImage = gradeImageMap['C']
+          } else {
+            // 默认情况，当大保底概率超出映射表的范围时，默认为C.png
+            gradeImage = gradeImageMap['C']
+          }
+          return gradeImage
+        }
+
+        async function addTextAndSpecialImagesToBackground(
+          text,
+          backgroundImagePath,
+          fontPath,
+          outputImagePath
+        ) {
+          try {
+            // 使用 Jimp 读取背景图片
+            const backgroundImage = await jimp.read(backgroundImagePath)
+            // 根据大保底概率选择合适的图片路径
+            const gradeImageFileName = await selectGradeImage(overallMisfitProbability)
+            const gradeImagePath = path.join(
+              `${process
+                .cwd()
+                .replace(
+                  /\\/g,
+                  '/'
+                )}/plugins/alemon-plugin-1999/resources/assets/img/抽卡分析/大保底/评级`,
+              gradeImageFileName
+            )
+
+            // 使用 Jimp 读取评级图片
+            const gradeImage = await jimp.read(gradeImagePath)
+
+            // 设置评级图片的合成位置
+            const gradeImageX = 966.2 // 替换为评级图片在背景图片上的 X 坐标
+            const gradeImageY = 1583.7 // 替换为评级图片在背景图片上的 Y 坐标
+
+            // 将评级图片合成在背景图片上
+            backgroundImage.composite(gradeImage, gradeImageX, gradeImageY)
+
+            // 生成包含文字的图片
+            const textImageBuffer = await generateTextImage(text, ttffontPath, 1069, 400)
+
+            // 使用 Jimp 读取文字图片
+            const textImage = await jimp.read(textImageBuffer)
+
+            // 设置文字图片的合成位置
+            const textImageX = 217 // 替换为文字图片在背景图片上的 X 坐标
+            const textImageY = 500 // 替换为文字图片在背景图片上的 Y 坐标
+
+            // 将文字图片合成在背景图片上
+            backgroundImage.composite(textImage, textImageX, textImageY)
+
+            // 保存合成后的图片
+            await backgroundImage.writeAsync(outputImagePath)
+            console.log('图片合成成功！')
+          } catch (error) {
+            console.error('图片合成失败：', error)
+          }
+          e.sendImage(
             `${process
               .cwd()
               .replace(
                 /\\/g,
                 '/'
-              )}/plugins/alemon-plugin-1999/resources/assets/img/抽卡分析/大保底/评级`,
-            gradeImageFileName
+              )}/plugins/alemon-plugin-1999/resources/assets/img/抽卡分析/大保底/baodi.jpg`
           )
-
-          // 使用 Jimp 读取评级图片
-          const gradeImage = await jimp.read(gradeImagePath)
-
-          // 设置评级图片的合成位置
-          const gradeImageX = 966.2 // 替换为评级图片在背景图片上的 X 坐标
-          const gradeImageY = 1583.7 // 替换为评级图片在背景图片上的 Y 坐标
-
-          // 将评级图片合成在背景图片上
-          backgroundImage.composite(gradeImage, gradeImageX, gradeImageY)
-
-          // 生成包含文字的图片
-          const textImageBuffer = await generateTextImage(text, ttffontPath, 1069, 400)
-
-          // 使用 Jimp 读取文字图片
-          const textImage = await jimp.read(textImageBuffer)
-
-          // 设置文字图片的合成位置
-          const textImageX = 217 // 替换为文字图片在背景图片上的 X 坐标
-          const textImageY = 500 // 替换为文字图片在背景图片上的 Y 坐标
-
-          // 将文字图片合成在背景图片上
-          backgroundImage.composite(textImage, textImageX, textImageY)
-
-          // 保存合成后的图片
-          await backgroundImage.writeAsync(outputImagePath)
-          console.log('图片合成成功！')
-        } catch (error) {
-          console.error('图片合成失败：', error)
         }
-        e.sendImage(
-          `${process
-            .cwd()
-            .replace(
-              /\\/g,
-              '/'
-            )}/plugins/alemon-plugin-1999/resources/assets/img/抽卡分析/大保底/baodi.jpg`
+        // 在图片上添加大保底概率文本并保存图片
+        const textToPrint = `${overallMisfitProbability.toFixed(2)}%`
+        const backgroundImagePath = `${process
+          .cwd()
+          .replace(
+            /\\/g,
+            '/'
+          )}/plugins/alemon-plugin-1999/resources/assets/img/抽卡分析/大保底/dbd.jpg` // 替换为背景图片路径
+        const ttffontPath = `${process
+          .cwd()
+          .replace(
+            /\\/g,
+            '/'
+          )}/plugins/alemon-plugin-1999/resources/assets/ttf/SourceHanSerifSC-VF.ttf` // 替换为外部字体文件的路径
+        const outputImagePath = `${process
+          .cwd()
+          .replace(
+            /\\/g,
+            '/'
+          )}/plugins/alemon-plugin-1999/resources/assets/img/抽卡分析/大保底/baodi.jpg`
+        addTextAndSpecialImagesToBackground(
+          textToPrint,
+          backgroundImagePath,
+          ttffontPath,
+          outputImagePath
         )
-      }
-      // 在图片上添加大保底概率文本并保存图片
-      const textToPrint = `${overallMisfitProbability.toFixed(2)}%`
-      const backgroundImagePath = `${process
-        .cwd()
-        .replace(
-          /\\/g,
-          '/'
-        )}/plugins/alemon-plugin-1999/resources/assets/img/抽卡分析/大保底/dbd.jpg` // 替换为背景图片路径
-      const ttffontPath = `${process
-        .cwd()
-        .replace(
-          /\\/g,
-          '/'
-        )}/plugins/alemon-plugin-1999/resources/assets/ttf/SourceHanSerifSC-VF.ttf` // 替换为外部字体文件的路径
-      const outputImagePath = `${process
-        .cwd()
-        .replace(
-          /\\/g,
-          '/'
-        )}/plugins/alemon-plugin-1999/resources/assets/img/抽卡分析/大保底/baodi.jpg`
-      addTextAndSpecialImagesToBackground(
-        textToPrint,
-        backgroundImagePath,
-        ttffontPath,
-        outputImagePath
-      )
+      })
     }
   }
 }
